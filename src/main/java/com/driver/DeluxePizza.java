2159f9347be7dee46ed29a97d70fec042846a3a7
Mohini56 <91308740+Mohini56@users.noreply.github.com>
2023-12-18 03:38:09 +0530

    Update DeluxePizza.java

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -2,10 +2,11 @@ package com.driver;
 
 public class DeluxePizza extends Pizza {
 
-    public DeluxePizza(String type) {
-        super(type);
-        // Deluxe pizza comes with extra cheese and toppings by default
-        addExtraCheese();
-        addExtraToppings();
+     public DeluxePizza(Boolean isVeg) {
+        super(isVeg);
+        // your code goes here
+        super.addExtraCheese();
+        super.addExtraToppings();
+
     }
 }
