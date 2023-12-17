657f230c1fe95c52482d91f4f6bbd19d942a66b9
Mohini56 <91308740+Mohini56@users.noreply.github.com>
2023-12-18 02:44:50 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,17 +2,78 @@ package com.driver;
 
 public class Main {
   public static void main(String[] args) {
-    DeluxePizza dp = new DeluxePizza(true);
-    System.out.println(dp.getPrice());
-    dp.addTakeaway();
-    dp.addExtraCheese();
-    dp.addTakeaway();
-    System.out.println(dp.getBill());
+     Scanner sc=new Scanner(System.in);
+        System.out.println("Would you like to eat Regular Pizza or Deluxe Pizza?");
+        String ans=sc.next();
 
-    Pizza p = new Pizza(true);
-    p.addExtraCheese();
-    p.addExtraToppings();
-    p.addTakeaway();
-    System.out.println(p.getBill());
+
+        System.out.println("Would you like to eat Veg Pizza or NonVeg Pizza?");
+        Pizza r = null;
+        DeluxePizza d;
+        String res=sc.next();
+        if(ans.equals("Regular")) {
+
+            if (res.equals("Veg")) {
+                r = new Pizza(true);
+            } else {
+                r = new Pizza(false);
+            }
+
+
+            System.out.println("Would you want Extra Cheese?");
+            String res1 = sc.next();
+            if (res1.equals("Yes")) {
+                r.extraCheese();
+            }
+
+            System.out.println("Would you want Extra Topping?");
+            String res2 = sc.next();
+            if (res2.equals("Yes")) {
+                r.extraTopping();
+            }
+
+            System.out.println("Would you want Paper bag?");
+            String res3=sc.next();
+            if(res3.equals("Yes")){
+                r.paperBag();
+            }
+
+            System.out.println("Total price of the Pizza is:"+ r.getTotalP());
+
+
+            System.out.println("Would you want to generate the bill?");
+            String res4=sc.next();
+            if(res4.equals("Yes")){
+                String ans1=r.bill();
+                System.out.println(ans1);
+            }
+
+        }
+        else{
+            if(res.equals("Veg")){
+                d=new DeluxePizza(true);
+
+            }
+            else{
+                d=new DeluxePizza(false);
+            }
+            System.out.println("Would you want Paper bag?");
+            String res3=sc.next();
+            if(res3.equals("Yes")){
+                d.paperBag();
+            }
+
+            System.out.println("Total price of the Pizza is:"+ d.getTotalP());
+
+
+            System.out.println("Would you want to generate the bill?");
+            String res4=sc.next();
+            if(res4.equals("Yes")){
+                String ans1=d.bill();
+                System.out.println(ans1);
+            }
+        }
+
+
   }
 }
