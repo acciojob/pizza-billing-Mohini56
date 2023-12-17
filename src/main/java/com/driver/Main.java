b2e0bfe216fb529b6b074584e765b65418b6da14
Mohini56 <91308740+Mohini56@users.noreply.github.com>
2023-12-18 03:25:48 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,18 +2,19 @@ package com.driver;
 
 public class Main {
   public static void main(String[] args) {
-    DeluxePizza dp = new DeluxePizza(true);
-    System.out.println(dp.getPrice());
-    dp.addTakeaway();
-    dp.addExtraCheese();
-    dp.addTakeaway();
-    System.out.println(dp.getBill());
+      Pizza vegPizza = new Pizza("Veg");
+        vegPizza.addExtraCheese();
+        vegPizza.addExtraToppings();
+        vegPizza.addPaperBag();
 
-    Pizza p = new Pizza(true);
-    p.addExtraCheese();
-    p.addExtraToppings();
-    p.addTakeaway();
-    System.out.println(p.getBill());
+        DeluxePizza deluxePizza = new DeluxePizza("Non-veg");
+        deluxePizza.addPaperBag();
+
+        int vegPizzaBill = vegPizza.calculateBill();
+        int deluxePizzaBill = deluxePizza.calculateBill();
+
+        System.out.println("Veg Pizza Bill: " + vegPizzaBill);
+        System.out.println("Deluxe Pizza Bill: " + deluxePizzaBill);
 
   }
 }
